6521c319dfa2937ae499b5a4f3ade8d90d79c44d
pc <crunchydeer30@gmail.com>
2024-02-25 12:02:28 +0300

    backend add /api/videos routes

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -4,6 +4,7 @@ import healthCheckRouter from './routes/healthcheck.routes';
 import accountsRouter from './routes/accounts.routes';
 import authRouter from './routes/auth.routes';
 import channelsRouter from './routes/channels.routes';
+import videosRouter from './routes/videos.routes';
 
 const apiRouter = Router();
 
@@ -11,5 +12,6 @@ apiRouter.use('/healthcheck', healthCheckRouter);
 apiRouter.use('/accounts', accountsRouter);
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/channels', channelsRouter);
+apiRouter.use('/videos', videosRouter);
 
 export default apiRouter;
---
+++ apps/server/src/routes/videos.routes.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router } from 'express';
+import videosController from '../controllers/videos.controller';
+import { auth } from '../middleware/auth';
+import upload from '../config/uploads';
+
+const videosRouter = Router();
+
+videosRouter.post(
+  '/upload',
+  auth.required,
+  upload.single('file'),
+  videosController.upload
+);
+
+export default videosRouter;
