aa5018da47bed7829d22049817cde6f7fbc4ed5b
pc <crunchydeer30@gmail.com>
2024-03-28 20:42:25 +0300

    client/feat: add some draft ui elements to videos page

--- apps/client/src/pages/Video.tsx
+++ apps/client/src/pages/Video.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { APITypes } from 'plyr-react';
 import VideoPlayer from '../features/videos/components/VideoPlayer';
-import Hls from 'hls.js';
 import 'plyr-react/plyr.css';
 import { useParams } from 'react-router-dom';
 
 const Video = () => {
   const ref = React.useRef<APITypes>(null);
-  const supported = Hls.isSupported();
   const { id } = useParams();
 
   const videoOptions = {
@@ -17,18 +15,28 @@ const Video = () => {
   const hlsSource = `https://b62f09b8-0d96-4852-ba07-aa435108b82f.selstorage.ru/${id}/master.m3u8`;
 
   return (
-    <div className="wrapper w-[800px]">
-      {supported ? (
-        <VideoPlayer
-          ref={ref}
-          source={videoSource}
-          options={videoOptions}
-          hlsSource={hlsSource}
-        />
-      ) : (
-        'HLS is not supported in your browser'
-      )}
-    </div>
+    <section className="px-4 py-8 max-w-[1700px] mx-auto">
+      <section className="w-full flex gap-8">
+        <section className="w-full rounded-xl overflow-hidden">
+          <VideoPlayer
+            ref={ref}
+            source={videoSource}
+            options={videoOptions}
+            hlsSource={hlsSource}
+          />
+        </section>
+        <section className="w-[400px] flex flex-col gap-4">
+          {Array(10)
+            .fill(0)
+            .map((_val, idx) => (
+              <div
+                key={idx}
+                className="bg-var-bg-secondary dark:bg-var-bg-secondary-dark w-full aspect-[21/9] rounded-xl"
+              ></div>
+            ))}
+        </section>
+      </section>
+    </section>
   );
 };
 
